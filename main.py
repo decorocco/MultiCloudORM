f666a6110c4f2da9279ae39327cceaa05de9e43e
decorocco <andrebr@al.insper.edu.br>
2021-11-26 16:18:07 -0300

    commit inicial main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import boto3
+
+iam = boto3.client("iam")
+
+for user in iam.list_users()["Users"]:
+    print("")
+    print(user["UserName"])
+    print(user["UserId"])
+    print(user["Arn"])
+    print(user["CreateDate"])
